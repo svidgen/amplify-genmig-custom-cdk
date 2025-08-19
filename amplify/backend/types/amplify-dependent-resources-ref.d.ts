006ad3236596abff131d8e4d16d6c7515df27af2
Jon Wire <iambipedal@gmail.com>
2025-08-19 09:24:51 -0500

    added auth and storage

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,1 +1,19 @@
-export type AmplifyDependentResourcesAttributes = {}
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "customcdkmigration795885a5": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  },
+  "storage": {
+    "migstorage": {
+      "BucketName": "string",
+      "Region": "string"
+    }
+  }
+}
